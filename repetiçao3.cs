01bc82657674651226b688bbc35ca344fc2716e5
luisK1ng <93940144+luisK1ng@users.noreply.github.com>
2022-07-10 21:15:25 -0300

    Add files via upload

---
+++ repetiçao3.cs
@@ -0,0 +1,15 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    int x=1; 
+    int y=1;
+    while (x <=10) {
+      Console.WriteLine(x*y);
+      x++;
+      y=-y;
+    }
+  }
+}
+  
