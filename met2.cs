d78797ba0b262abc822ba667a95318ca55153fe9
luisK1ng <93940144+luisK1ng@users.noreply.github.com>
2022-07-29 10:28:03 -0300

    Add files via upload

---
+++ met2.cs
@@ -0,0 +1,11 @@
+using System;
+public class Program{
+  public static void Main(string[] args){
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine(AreaCirculo(a));
+  }
+  public static double AreaCirculo(double raio){
+    return 3.14 * Math.Pow(raio, 2);
+    
+  }
+}
